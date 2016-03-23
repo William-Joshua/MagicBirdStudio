8561732ab4e2d47a7545a18bacf208955116d94c
William-Joshua <njl.yxsky@gmail.com>
2016-03-23 22:50:36 +0800

    重新上传

---
+++ KO_ERP_INVENTORY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KO_ERP_INVENTORY.Startup))]
+namespace KO_ERP_INVENTORY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
