7ae9920329c5b23d8fdce2adc00457f99f043f47
William-Joshua <yourenlaila@hotmail.com>
2016-10-10 12:20:44 +0800

    Date:2016-10-10;Author : William.D.Joshua;Version : 0.1.0.01;Description : 部署 Magic Brid Studio 开发环境

---
+++ MagicBirdStudio/App_Start/BundleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MagicBirdStudio
+{
+    public class BundleConfig
+    {
+        // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Bootstrap Core CSS
+            bundles.Add(new StyleBundle("~/Content/Bootstrap").Include(
+                "~/Content/Components/bootstrap/dist/css/bootstrap.min.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/Components/metisMenu").Include(
+                "~/Content/Components/metisMenu/dist/metisMenu.min.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/Timeline").Include(
+                "~/Content/Styles/css/timeline.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/sb-admin-2").Include(
+                "~/Content/Styles/css/sb-admin-2.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/Font").Include(
+                "~/Content/Components/font-awesome/css/font-awesome.min.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/morris").Include(
+                "~/Content/Components/morrisjs/morris.css"
+                ));
+            bundles.Add(new ScriptBundle("~/Script/Jquery").Include(
+                "~/Content/Components/jquery/dist/jquery.min.js"
+                ));
+            bundles.Add(new ScriptBundle("~/Script/bootstrap").Include(
+                "~/Content/Components/bootstrap/dist/js/bootstrap.min.js"
+                ));
+            bundles.Add(new ScriptBundle("~/Script/metisMenu").Include(
+                "~/Content/Components/metisMenu/dist/metisMenu.min.js"
+                ));
+            bundles.Add(new ScriptBundle("~/Script/raphael").Include(
+                "~/Content/raphael/raphael-min.js"
+                ));
+            bundles.Add(new ScriptBundle("~/Script/morris").Include(
+                "~/Content/Components/morrisjs/morris.min.js"
+                ));
+            bundles.Add(new ScriptBundle("~/Script/sb-admin-2").Include(
+                "~/Content/Scripts/js/sb-admin-2.js"
+                ));
+        }
+    }
+}
---
+++ MagicBirdStudio/Document/MVC Program Beginning.md
@@ -0,0 +1,24 @@
+###Asp.net开发框架学习 : MVC + EF + BootStarp
+
+####Section 0: Development Environment Instructions
+| id | softwave&environment | vesion | remark |
+|--------|--------|--------|--------|
+| 1 | Microsoft Windows 10 专业版 | Version 1511 |  |
+| 2 | Microsoft Visual Studio Ultimate 2012 | Version 11.0.61219.00 Update 5 ||
+| 3 | Microsoft .NET FrameWork | Version 4.6.01038 ||
+| 4 | Microsoft ASP.NET MVC 4 | Version 4.0.20710.0  |  |
+| 5 | MySql | Version 14.14 Distrib 5.7.14, for Win64 (x86_64) ||
+| 6 | NuGet Package Manager | Version 2.8.60318.667 | 软件包管理软件 |
+| 7 | EntityFramework | Version 6.0.0 |  |
+| 8 | MySql.Data/MySql.Data.Entity | Version 6.9.9.0 | .net 连接 Mysql 数据库 |
+| 9 | Microsoft ASP.NET Web Optimization | Version 1.1.3 |  |
+| 10 | Git  | git version 2.9.0.windows.1 | 版本管理软件 |
+| 11  | Visual Studio Tools for Git | Version 1.0.1.0 |  |
+| 12 | Json.NET | Version 5.04 ||
+| 13 | Microsoft ASP.NET Razor 2 | Version 2.0.20715.0 ||
+
+- - -
+
+####Section 1 : Build project with MVC + EF + bootstap in VS2015
+
+_ _ _
